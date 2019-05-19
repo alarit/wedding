3aaff652a81c0fc5fe25532be80a5e6cff3849d6
Alessandro Arici <a.arici@techgap.it>
2019-05-19 11:35:12 +0200

    Refactoring layout, structure...pretty everything

---
+++ js/index.js
@@ -0,0 +1,29 @@
+function openMap() {
+    window.open('https://goo.gl/maps/szyJkir5eBcTzL3EA', '_blank');
+}
+
+function openRestaurantSite() {
+    window.open('https://www.tenutacollepiajo.it/', '_blank');
+}
+
+function openTravelAgancyMap() {
+    window.open('http://www.chakraviaggi.it', '_blank');
+}
+
+function showChurch() {
+    document.getElementById('church').style.display = 'block'
+    document.getElementById('restaurant').style.display = 'none'
+    document.getElementById('agency').style.display = 'none'
+}
+
+function showRestaurant() {
+    document.getElementById('church').style.display = 'none'
+    document.getElementById('restaurant').style.display = 'block'
+    document.getElementById('agency').style.display = 'none'
+}
+
+function showAgency() {
+    document.getElementById('church').style.display = 'none'
+    document.getElementById('restaurant').style.display = 'none'
+    document.getElementById('agency').style.display = 'block'
+}
